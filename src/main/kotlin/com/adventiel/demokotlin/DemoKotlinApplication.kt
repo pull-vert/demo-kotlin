fd87b5de3564b91e4d429937517e5991887557e2
pull-vert <frederic.montariol@gmail.com>
2018-03-04 23:22:20 +0100

    spring initializer direct commit

---
+++ src/main/kotlin/com/adventiel/demokotlin/DemoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.adventiel.demokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoKotlinApplication>(*args)
+}
