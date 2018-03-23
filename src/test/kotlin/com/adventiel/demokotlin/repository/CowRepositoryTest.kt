b9dc28d8c420254a97ef44806e12837086d344fe
frederic.montariol <frederic.montariol@adventiel.fr>
2018-03-23 17:02:54 +0100

    polishing : unused import

--- src/test/kotlin/com/adventiel/demokotlin/repository/CowRepositoryTest.kt
+++ src/test/kotlin/com/adventiel/demokotlin/repository/CowRepositoryTest.kt
@@ -1,7 +1,6 @@
 package com.adventiel.demokotlin.repository
 
 import org.assertj.core.api.Assertions.assertThat
-import org.hamcrest.core.IsNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
