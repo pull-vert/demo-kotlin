dd07adf10e5a0a75ffb84d620f17469419326008
pull-vert <frederic.montariol@gmail.com>
2018-11-20 04:07:14 +0100

    add spring rest docs, tests do not use HTTP/2 but HTTP/1.1

--- src/main/kotlin/demo/kotlin/web/ApiRoutes.kt
+++ src/main/kotlin/demo/kotlin/web/ApiRoutes.kt
@@ -3,18 +3,44 @@ package demo.kotlin.web
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType.APPLICATION_JSON
-import org.springframework.web.reactive.function.server.router
+import org.springframework.web.reactive.function.server.*
+
 
 @Configuration
 class ApiRoutes(val cowHandler: CowHandler) {
 
     @Bean
-    fun appRouter() = router {
+    fun appRouter(/*httpsRedirectFilter: HandlerFilterFunction<ServerResponse, ServerResponse>*/) = router {
         accept(APPLICATION_JSON).nest {
             "/api/cows".nest {
                 GET("/{name}", cowHandler::findByName)
                 GET("/", cowHandler::findAll)
             }
         }
-    }
+    }/*.filter(httpsRedirectFilter)
+
+    @Bean
+    fun httpsRedirectFilter() = object: HandlerFilterFunction<ServerResponse, ServerResponse> {
+        override fun filter(request: ServerRequest, next: HandlerFunction<ServerResponse>): Mono<ServerResponse> {
+                        val originalUri = request.uri()
+
+//            here set your condition to http->https redirect
+            val forwardedValues = request.headers().header("x-forwarded-proto")
+            if (forwardedValues.contains("http")) {
+                try {
+                    val mutatedUri = URI("https",
+                            originalUri.userInfo,
+                            originalUri.host,
+                            originalUri.port,
+                            originalUri.path,
+                            originalUri.query,
+                            originalUri.fragment)
+                    return ServerResponse.permanentRedirect(mutatedUri).build()
+                } catch (e: URISyntaxException) {
+                    throw IllegalStateException(e.message, e)
+                }
+            }
+        return next.handle(request)
+        }
+    }*/
 }
