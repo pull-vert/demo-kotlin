ca46bfb0e63501649148efe21134406c9e0aedaa
pull-vert <frederic.montariol@gmail.com>
2019-01-15 17:40:43 +0100

    fix and complete JWTUtil tests

--- src/main/kotlin/demo/kotlin/security/JWTUtil.kt
+++ src/main/kotlin/demo/kotlin/security/JWTUtil.kt
@@ -1,5 +1,6 @@
 package demo.kotlin.security
 
+import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
@@ -14,11 +15,13 @@ class JWTUtil(
         @Value("\${jwt.expiration}") private val expirationTime: String
 ) {
 
-    fun getAllClaimsFromToken(token: String) = Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(token).body
+    private val jwtParser = Jwts.parser().setSigningKey(secret.toByteArray());
+
+    fun getAllClaimsFromToken(token: String) = jwtParser.parseClaimsJws(token).body
 
     fun getUsernameFromToken(token: String): String = getAllClaimsFromToken(token).subject
 
-    fun getExpirationDateFromToken(token: String) = getAllClaimsFromToken(token).expiration
+//    fun getExpirationDateFromToken(token: String) = getAllClaimsFromToken(token).expiration
 
     fun generateToken(user: UserDetails): String {
         val claims = mutableMapOf<String, Any>()
@@ -27,7 +30,17 @@ class JWTUtil(
         return doGenerateToken(user.getUsername(), claims)
     }
 
-    fun validateToken(token: String) = !isTokenExpired(token, Date())
+    fun validateToken(token: String): Boolean {
+        try {
+            // token must be signed
+            if (!jwtParser.isSigned(token)) return false
+            // parse token to get claims, will throw ExpiredJwtException if expired at current time
+            val claims = getAllClaimsFromToken(token)
+            return claims.getOrDefault("enable", false) as Boolean
+        } catch(ex: ExpiredJwtException) {
+            return false
+        }
+    }
 
     private fun doGenerateToken(username: String, claims: Map<String, Any>): String {
         val expirationTimeLong = expirationTime.toLong() //in second
@@ -41,10 +54,5 @@ class JWTUtil(
                 .setExpiration(expirationDate)
                 .signWith(SignatureAlgorithm.HS512, secret.toByteArray())
                 .compact()
-    }
-
-    internal fun isTokenExpired(token: String, date: Date): Boolean {
-        val expiration = getExpirationDateFromToken(token)
-        return expiration.before(date)
     }
 }
