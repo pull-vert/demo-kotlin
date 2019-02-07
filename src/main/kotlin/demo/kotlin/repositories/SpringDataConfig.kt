657e6bfe05ce0d80ca97f3dca7a737ed142160d5
pull-vert <frederic.montariol@gmail.com>
2019-02-07 18:05:03 +0100

    create SpringDataConfig @Configuration

---
+++ src/main/kotlin/demo/kotlin/repositories/SpringDataConfig.kt
@@ -0,0 +1,8 @@
+package demo.kotlin.repositories
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+
+@Configuration
+@EnableMongoAuditing
+class SpringDataConfig
--- src/main/kotlin/demo/kotlin/web/WebConfig.kt
+++ src/main/kotlin/demo/kotlin/web/WebConfig.kt
@@ -1,9 +1,8 @@
-package demo.kotlin
+package demo.kotlin.web
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.http.codec.json.Jackson2JsonEncoder
@@ -12,8 +11,7 @@ import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
 @EnableWebFlux
-@EnableMongoAuditing
-class WebConfiguration (private val objectMapper: ObjectMapper) : WebFluxConfigurer {
+class WebConfig (private val objectMapper: ObjectMapper) : WebFluxConfigurer {
 
     /**
      * Configure Jackson to Serialize and Deserialize dates with ISO-8601 format
