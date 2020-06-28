cbefb149ee0292e6090d522a9828be89f400f20c
pull-vert <5107035+pull-vert@users.noreply.github.com>
2020-06-28 12:33:17 +0200

    must have a Role empty constructor

--- src/main/kotlin/demo/kotlin/entities/Role.kt
+++ src/main/kotlin/demo/kotlin/entities/Role.kt
@@ -3,9 +3,9 @@ package demo.kotlin.entities
 import org.springframework.security.core.GrantedAuthority
 import java.util.*
 
-enum class Role(override val id: UUID) : GrantedAuthority, Entity {
-    ROLE_USER(UUID.randomUUID()), // fixme : use fixed UUID
-    ROLE_ADMIN(UUID.randomUUID());
+enum class Role(override val id: UUID = UUID.randomUUID()) : GrantedAuthority, Entity {
+    ROLE_USER, // fixme : use fixed UUID
+    ROLE_ADMIN;
 
     override fun getAuthority() = this.name
 }
