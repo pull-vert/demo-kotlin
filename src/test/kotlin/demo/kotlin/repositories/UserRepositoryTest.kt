145eaf60bc88415db990d7d8dcd11fad80932938
pull-vert <5107035+pull-vert@users.noreply.github.com>
2020-07-01 14:28:17 +0200

    UserRepositoryTest tests are back !

--- src/test/kotlin/demo/kotlin/repositories/UserRepositoryTest.kt
+++ src/test/kotlin/demo/kotlin/repositories/UserRepositoryTest.kt
@@ -1,8 +1,29 @@
 package demo.kotlin.repositories
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import reactor.kotlin.test.test
 
 @SpringBootTest
 class UserRepositoryTest(@Autowired private val userRepository: UserRepository) {
+
+    @Test
+    fun `Verify findByUsername returns existing Fred User`() {
+        userRepository.findByUsername("Fred")
+                .test()
+                .consumeNextWith { user ->
+                    assertThat(user.username).isEqualTo("Fred")
+                    assertThat(user.password).isNotNull()
+                    assertThat(user.id).isNotNull()
+                }.verifyComplete()
+    }
+
+    @Test
+    fun `Verify findByUsername returns no John User`() {
+        userRepository.findByUsername("John")
+                .test()
+                .verifyComplete()
+    }
 }
