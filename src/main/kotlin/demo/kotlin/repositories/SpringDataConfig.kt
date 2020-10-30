c4ce13bb78331f98cd27606495558eaad5ee6c9f
pull-vert <5107035+pull-vert@users.noreply.github.com>
2020-10-30 22:56:58 +0100

    update dependencies, including Kotysa 0.1.8

--- src/main/kotlin/demo/kotlin/repositories/SpringDataConfig.kt
+++ src/main/kotlin/demo/kotlin/repositories/SpringDataConfig.kt
@@ -2,7 +2,7 @@ package demo.kotlin.repositories
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.DatabaseClient
 import org.ufoss.kotysa.r2dbc.sqlClient
 
 
